6770af594de94f8437aff61a9bd31a8098378fec
mir809 <humir809@naver.com>
2023-03-29 06:49:51 +0900

    #4.0 ~ #4.7 코드 챌린지 모범답안

--- challenge/src/index.js
+++ challenge/src/index.js
@@ -1,45 +1,22 @@
-const generater = document.querySelector(".generate-number");
-const generaterInput = document.querySelector(".generate-number input");
-const guess = document.querySelector(".guess-number");
-const guessInput = document.querySelector(".guess-number input");
-const chose = document.querySelector(".chose");
-const result = document.querySelector(".result");
+const guessForm = document.getElementById("js-guess");
+const result = document.getElementById("js-result");
+const maxNumber = document.getElementById("maxNumber");
 
-function select(submit) {
-  submit.preventDefault();
-  const setNumber = parseInt(generaterInput.value);
-  const randomNumber = Math.ceil(Math.random() * setNumber);
-  const selectNumber = parseInt(guessInput.value);
-
-  if (isNaN(setNumber)) {
-    chose.innerText = `Please Input Generate a number`;
-  } else {
-    chose.innerText = `You chose: ${selectNumber}, the machine chose: ${randomNumber}.`;
-    if (selectNumber === randomNumber) {
-      result.innerText = `You won!`;
-    } else if (selectNumber !== randomNumber) {
-      result.innerText = `You lost!`;
-    }
+function handleGuessSubmit(e) {
+  e.preventDefault();
+  const guessInput = guessForm.querySelector("input");
+  if (guessInput.value === "" && maxNumber === "") {
+    return;
   }
+  const max = maxNumber.value;
+  const random = Math.ceil(Math.random() * max);
+  const userGuess = parseInt(guessInput.value, 10);
+  const resultSpan = result.querySelector("span");
+  resultSpan.innerHTML = `
+  You chose: ${userGuess},
+  the machine chose: ${random}.<br />
+  <strong>${userGuess === random ? "You won!" : "You lost!"}</strong>
+  `;
 }
 
-function random(submit) {
-  submit.preventDefault();
-  const setNumber = parseInt(generaterInput.value);
-  const randomNumber = Math.ceil(Math.random() * setNumber);
-  const selectNumber = parseInt(guessInput.value);
-
-  if (isNaN(selectNumber)) {
-    chose.innerText = `Please Input Guess the number`;
-  } else {
-    chose.innerText = `You chose: ${selectNumber}, the machine chose: ${randomNumber}.`;
-    if (selectNumber === randomNumber) {
-      result.innerText = `You won!`;
-    } else if (selectNumber !== randomNumber) {
-      result.innerText = `You lost!`;
-    }
-  }
-}
-
-generater.addEventListener("submit", random);
-guess.addEventListener("submit", select);
+guessForm.addEventListener("submit", handleGuessSubmit);
